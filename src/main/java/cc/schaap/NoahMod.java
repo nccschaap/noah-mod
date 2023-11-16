9f4097b4258434da85a599e06f9ec099a83af165
Bastiaan Schaap <bastiaan.schaap@siteminds.nl>
2023-11-16 22:25:44 +0100

    Add noah gem

--- src/main/java/cc/schaap/NoahMod.java
+++ src/main/java/cc/schaap/NoahMod.java
@@ -1,5 +1,6 @@
 package cc.schaap;
 
+import cc.schaap.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,8 @@ public class NoahMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Starting " + MOD_ID);
+		ModItems.registerModItems();
+
 	}
 }
